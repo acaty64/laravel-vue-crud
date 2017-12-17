f4b0632b22a0db08b1e9256ea49eba948abe5e70
acaty64 <ana.arashiro@gmail.com>
2017-12-17 04:20:59 +0000

    Formulario de Crear Lesson 7

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,9 @@
 		<h1 class="page-header">CRUD LARAVEL y VUEjs</h1>
 	</div>
 	<div class="col-sm-7">
-		<a href="#" class="btn btn-primary float-right">Nueva Tarea</a>
+		<a href="#" class="btn btn-primary pull-right" data-toggle="modal" data-target="#create">
+			Nueva Tarea
+		</a>
 		<table class="table table-striped table-hover">
 			<thead>
 				<tr>
@@ -31,6 +33,7 @@
 				</tr>
 			</tbody>
 		</table>		
+		@include('create')
 	</div>
 	<div class="col-sm-5">
 		<pre>
